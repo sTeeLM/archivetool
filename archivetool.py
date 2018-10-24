2df83e555dce574f6c13c1e1be84de3eb997b682
sTeeLM <steel.mental@gmail.com>
2018-10-24 19:26:36 +0800

    bug fix

--- archivetool.py
+++ archivetool.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
-import os,subprocess,sys
+import os,subprocess,sys,getopt
 
 # global conf
 ##############################################
-rar_cmd = r"C:\Program Files\WinRAR\rar.exe"
-unlockrar_cmd = r"C:\Program Files\WinRAR\RAR Unlocker.exe"
+rar_cmd = r'C:\Program Files\WinRAR\rar.exe'
+unlockrar_cmd = r'C:\Program Files\WinRAR\RAR Unlocker.exe'
 file_stdout = r'C:\Data\Test\stdout.log'
 file_stderr = r'C:\Data\Test\stderr.log'
 file_status = r'C:\Data\Test\status.log'
+
 #############################################
 
 def test_rar_recover(pathname):
@@ -20,85 +21,123 @@ def test_rar_recover(pathname):
 	return 0
 
 
-def find_nonrar(pathname, fstdout, fstderr, fstatus):
+def list_nonrar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
 	if ext[1].upper() == '.RAR' :
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
 	else :
 		print pathname
-		fstatus.write("notrar,%s\n" % (pathname))
+		fstatus.write('notrar,%s\n' % (pathname))
 
-def unlock_rar(pathname, fstdout, fstderr, fstatus):
+def unlock_rar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
 	if ext[1].upper() == '.RAR' :
-		status = subprocess.call([unlockrar_cmd, "--unlock", pathname], stdout=fstdout, stderr=fstderr, shell=False)
-		print "unlocking %s : %d" % (pathname, status)
-		fstatus.write("%d,%s\n" % (status, pathname))
+		status = subprocess.call([unlockrar_cmd, '--unlock', pathname], stdout=fstdout, stderr=fstderr, shell=False)
+		print 'unlocking %s : %d' % (pathname, status)
+		fstatus.write('%d,%s\n' % (status, pathname))
 	else:
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
 
-def lock_rar(pathname, fstdout, fstderr, fstatus):
+def lock_rar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
 	if ext[1].upper() == '.RAR' :
-		status = subprocess.call([rar_cmd, "k", pathname], stdout=fstdout, stderr=fstderr, shell=False)
-		print "locking %s : %d" % (pathname, status)
-		fstatus.write("%d,%s\n" % (status, pathname))
+		status = subprocess.call([rar_cmd, 'k', pathname], stdout=fstdout, stderr=fstderr, shell=False)
+		print 'locking %s : %d' % (pathname, status)
+		fstatus.write('%d,%s\n' % (status, pathname))
 	else:
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
 
-def test_rar(pathname, fstdout, fstderr, fstatus):
+def test_rar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
 	if ext[1].upper() == '.RAR' :
-		status = subprocess.call([rar_cmd, "t", pathname], stdout=fstdout, stderr=fstderr, shell=False)
-		print "testing %s : %d" % (pathname, status)
-		fstatus.write("%d,%s\n" % (status, pathname))
+		status = subprocess.call([rar_cmd, 't', pathname], stdout=fstdout, stderr=fstderr, shell=False)
+		print 'testing %s : %d' % (pathname, status)
+		fstatus.write('%d,%s\n' % (status, pathname))
 	else:
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
 
-def addrecover_rar(pathname, fstdout, fstderr, fstatus):
+def addrecover_rar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
-	if ext[1].upper() == '.RAR' and test_rar_recover(pathname) == 0:
-		status = subprocess.call([rar_cmd, "rr5p", pathname], stdout=fstdout, stderr=fstderr, shell=False)
-		print "adding recover record %s : %d" % (pathname, status)
-		fstatus.write("%d,%s\n" % (status, pathname))
+	rropt = 'rr%dp' % (option['recovery_percent'])
+	if ext[1].upper() == '.RAR':
+		if option['force'] or test_rar_recover(pathname) == 0 :
+			status = subprocess.call([rar_cmd, rropt, pathname], stdout=fstdout, stderr=fstderr, shell=False)
+			print 'adding recover record %s : %d' % (pathname, status)
+			fstatus.write('%d,%s\n' % (status, pathname))
+		else:
+			fstatus.write('skip,%s\n' % (pathname))
 	else:
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
 
-def create_rar(pathname, fstdout, fstderr, fstatus):
+def create_rar(pathname, fstdout, fstderr, fstatus, option):
 	ext = os.path.splitext(pathname)
+	rropt = '-rr%dp' % (option['recovery_percent'])
+	volopt = '-v' + option['volume_size']
 	if ext[1].upper() != '.RAR' and pathname.upper() != file_status.upper() and pathname.upper() != file_stdout.upper() and pathname.upper() != file_stderr.upper():
 		rar_name = ext[0] + '.rar'
-		status = subprocess.call([rar_cmd, "a", "-rr5p", "-ep", "-o-", rar_name, pathname], stdout=fstdout, stderr=fstderr, shell=False)
-		print "create archive %s : %d" % (pathname, status)
-		fstatus.write("%d,%s\n" % (status, pathname))
+		status = subprocess.call([rar_cmd, 'a', rropt, volopt, '-ep', '-o-', '-df', rar_name, pathname], stdout=fstdout, stderr=fstderr, shell=False)
+		print 'create archive %s : %d' % (pathname, status)
+		fstatus.write('%d,%s\n' % (status, pathname))
 	else:
-		fstatus.write("skip,%s\n" % (pathname))
+		fstatus.write('skip,%s\n' % (pathname))
+
+def usage():
+	print 'archivetool.py cmd <options> directory'
+	print '    test: test if rar files corrupted'
+	print '    lock: lock rar files'
+	print '    unlock: unlock rar files'
+	print '    addrecover <-f|-force> <-r|--recovery-percent=num>, add recovery information:'
+	print '        default not forced, recovery percent 5'
+	print '    create <-r|--recovery-percent=num> <-v|--volume-size=num|kKmMgG>: make all non-rar files into rar'
+	print '        default recovery percent 5, volume size 4g'
+	print '    listnonrar: find all non rar files'
+
+def main(argv) :
+	global file_stdout, file_stderr, file_status
+	fun_hash = {'test' : test_rar, 'lock' : lock_rar, 'unlock': unlock_rar, 'addrecover' : addrecover_rar, 'create': create_rar, 'listnonrar': list_nonrar}
+	options_hash = {'volume_size':'4g', 'force':False, 'recovery_percent':5}
+	try:
+		opts,args = getopt.getopt(sys.argv[2:], 'v:r:hf', ['volume-size=','recovery-percent=','help', 'force'])
+		for opt,arg in opts:
+			if opt in ('-h', '--help'):
+				print 'show help'
+				usage()
+				sys.exit(0)
+			elif opt in ('-v', '--volume-size'):
+				options_hash['volume_size'] = arg
+			elif opt in ('-f', '--force'):
+				options_hash['force'] = True
+			elif opt in ('-r', '--recovery-percent'):
+				options_hash['recovery_percent'] = int(arg)
+			else:
+				print 'unknown option %s' % (opt)
+				usage()
+				sys.exit(1)
+
+		if len(args) != 1 or not fun_hash.has_key(argv[1]):
+			usage()
+			sys.exit(1)
+		else:
+			directory = args[0]
+			cmd = argv[1]
+			print 'directory is %s, cmd is %s' % (directory, cmd)
+			print 'options is %s' % (options_hash)
+	except getopt.GetoptError:
+		usage()
+		sys.exit(1)
+
+	log_stdout = open(file_stdout,'w')
+	log_stderr = open(file_stderr,'w')
+	log_status = open(file_status,'w')
+	
+	for dirpath,dirnames,filenames in os.walk(directory):
+		for file in filenames:
+			fullpath=os.path.join(dirpath,file)
+			fun_hash[argv[1]](fullpath, log_stdout, log_stderr, log_status, options_hash)
+
+	log_status.close()
+	log_stdout.close()
+	log_stderr.close()
 
 
-log_stdout = open(file_stdout,'w')
-log_stderr = open(file_stderr,'w')
-log_status = open(file_status,'w')
-
-cmd = sys.argv[1]
-path= sys.argv[2]
-
-
-for dirpath,dirnames,filenames in os.walk(path):
-	for file in filenames:
-		fullpath=os.path.join(dirpath,file)
-		if cmd == 'test' :
-			test_rar(fullpath, log_stdout, log_stderr, log_status)
-		elif cmd == 'lock' :
-			lock_rar(fullpath, log_stdout, log_stderr, log_status)
-		elif cmd == 'unlock' :
-			unlock_rar(fullpath, log_stdout, log_stderr, log_status)
-		elif cmd == 'addrecover' :
-			addrecover_rar(fullpath, log_stdout, log_stderr, log_status)
-		elif cmd == 'create' :
-			create_rar(fullpath, log_stdout, log_stderr, log_status)
-		elif cmd == 'findnonrar' :
-			find_nonrar(fullpath, log_stdout, log_stderr, log_status)
-
-log_status.close()
-log_stdout.close()
-log_stderr.close()
+main(sys.argv)
